566ee552abcc17b76a2030ee3ec7b09de8aad05e
geraldsanga <gerrysanga33@protonmail.ch>
2025-03-01 23:11:03 +0300

    Update
    1. Commited the rest of forgotten project files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', redirectTo: '' }
+];
